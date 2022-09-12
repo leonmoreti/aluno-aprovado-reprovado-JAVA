93757723483f67100d278e60cd58074dcf515936
leonmoreti <leonmoreti7@gmail.com>
2022-09-12 11:53:16 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int nota = 69;
+
+
+        //nota maior ou igual 70, então aprovado
+        //if-else
+        if (nota >= 70) {
+            System.out.println("Aluno aprovado!");
+        } else {
+            System.out.println("Aluno reprovado!");
+
+        }
+
+
+
+
+
+    }
+}
